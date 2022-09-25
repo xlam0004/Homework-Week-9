e863a80278d484fb3daebb7d944a66ff929e8e5f
xlam0004 <113871296+xlam0004@users.noreply.github.com>
2022-09-25 11:26:59 +0800

    Add files via upload
    added all

---
+++ prop_symbol_map.js
@@ -0,0 +1,4 @@
+var vg_1 = "prop_symbol_map.vg.json";
+
+// display at the bar_chart division, JSON file name
+vegaEmbed("#propotional_symbol_map", vg_1).then(function(result){}).catch(console.error);
