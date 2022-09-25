7180bdac01b490f4c84607616d9981c0948764ff
xlam0004 <113871296+xlam0004@users.noreply.github.com>
2022-09-25 12:45:02 +0800

    Update prop_symbol_map.js

--- prop_symbol_map.js
+++ prop_symbol_map.js
@@ -1,4 +1,4 @@
-var vg_1 = "prop_symbol_map.vg.json";
+var vg_1 = "https://raw.githubusercontent.com/xlam0004/Homework-Week-9/main/prop_symbol_map.vg.json";
 
 // display at the bar_chart division, JSON file name
 vegaEmbed("#propotional_symbol_map", vg_1).then(function(result){}).catch(console.error);
